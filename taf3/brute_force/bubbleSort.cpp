b86733f346753166778a61b133afa3bf1608a26d
Tiago Albuquerque Ferreira <tiagoferreira@MacBook-Air-de-Tiago.local>
2025-09-11 19:16:49 -0300

    Bubble sort - corrigido

--- taf3/brute_force/bubbleSort.cpp
+++ taf3/brute_force/bubbleSort.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main()
 {
 
-    vector<int> list = {3, 4, 7, 1, 2, 5, 8, 9};
+    vector<int> list = {3, 4, 7, 9, 2, 5, 8, 1};
 
-    for (int i = 0; i < list.size() - 2; i++)
+    for (int i = 0; i < list.size() - 1; i++)
     {
-        for (int j = 0; j < list.size() - 2 - i; j++)
+        for (int j = 0; j < list.size() - 1 - i; j++)
         {
-            if (list[j + 1] < list[i])
+            if (list[j + 1] < list[j])
             {
                 int copy = list[j];
                 list[j] = list[j + 1];
