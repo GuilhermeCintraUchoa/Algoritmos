614f85a1685e1029d88e15bc7902155d976def9e
GuilhermeCintraUchoa <cintratop@gmail.com>
2025-09-12 13:00:29 -0300

    terminei o bubble sort direito, falta comentar

--- gcucc/BruteForce/bubble-sort.c
+++ gcucc/BruteForce/bubble-sort.c
@@ -1,41 +1,41 @@
-// ACHO QUE FIZ UM BUBBLE SORT SEM QUERER
 #include <stdio.h>
 
 void print_array_atual(int array[], int tamanho_da_array);
 
 int main(void) {
-
-  int A[] = {89, 45, 89, 90, 29, 34, 17};
-  int size_of_array = sizeof(A);
-  int size_of_int = sizeof(int);
-  int tamanho = size_of_array/size_of_int;
-
-  for (int i = 0; i < tamanho; i++){
-      printf("%d ", *(A+i));
+  int A[] = {89, 45, 68, 90, 29, 34, 17};
+  int tamanho_lista = sizeof(A)/sizeof(A[0]);
+  //quero que se o elemento da frente for maior que o presente, troque, e repita ate o fim do ciclo desse elemento
+  for (int i = 0; i < tamanho_lista; i++){
+    printf("%d ", A[i]);
   }
+
   printf("\n");
 
-  for (int i = 0; i < tamanho-1; i++){
-    for (int j = i+1; j < tamanho; j++){
-      int tmp;
-      if(A[i] > A[j]){
-        //swap
-        tmp = A[i];
-        A[i] = A[j];
+  for (int i = 0; i < tamanho_lista - 1; i++){
+    int tmp;
+    for (int j = 1; j < tamanho_lista - i; j++){
+      if(A[j-1] > A[j]){
+        tmp = A[j-1];
+        A[j-1] = A[j];
         A[j] = tmp;
       }
+      for (int i = 0; i < tamanho_lista; i++){
+        if(A[i] == tmp){
+          printf("*");
+        }
+        printf("%d ", A[i]);
+      }
+      printf("\n");
     }
-    print_array_atual(A, tamanho);
-    
-  }
-  // for (int i = 0; i < tamanho; i++){
-    // printf("A[%d]: %d\n", i, A[i]);
-  // }
-}
 
-void print_array_atual(int array[], int tamanho_da_array){
-  for (int i = 0; i < tamanho_da_array; i++){
-    printf("%d ", *(array+i));
+  }
+
+
+  for (int i = 0; i < tamanho_lista; i++){
+    printf("A[%d]: %d ", i, A[i]);
   }
   printf("\n");
+
+  return 0;
 }
