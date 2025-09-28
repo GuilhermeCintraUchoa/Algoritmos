aa23d737a244b78cbaba2142db9a4b7870ec75ea
GuilhermeCintraUchoa <cintratop@gmail.com>
2025-09-28 13:50:23 -0300

    terminei mergesort

--- gcucc/DivideAndConquer/mergesort.cpp
+++ gcucc/DivideAndConquer/mergesort.cpp
@@ -3,32 +3,67 @@
 
 using namespace std;
 
-vector<int> mergeSort(vector<int>);
+void mergeSort(vector<int>& A, int l, int r);
+void merge(vector<int>& A, int l, int r);
 
 int main(void){
-    int n,input;
+    vector<int> A = {5, 2, 1, 7, 0};
 
-    cin >> n;
+    mergeSort(A, 0, 4);
 
-    vector<int> A;
-    vector<int> B;
-    vector<int> C;
-
-
-    for(int i = 0; i < n; i++){
-        cin >> input;
-        A.push_back(input);
-    }
-
-    else{
-        cout<<A[0]<<"\n";
-    }
     
-    // for(int i : A){
-    //     cout<<i<<" ";
-    // }
+    for(int p : A){
+        cout << p << " ";
+    }
+    cout << "\n";
 
-    // cout<<"\n";
 }
 
-vector<int> mergeSort(vector<int>){}
+void mergeSort(vector<int>& A, int l, int r){
+    if(l < r){ //preciso checar se a array nao eh unitaria. Se for unitaria esta ordenada por definicao.
+        int m = (l+r)/2;
+
+        mergeSort(A, l, m);
+        mergeSort(A, m+1, r);
+        merge(A, l, r);
+    }
+}
+
+void merge(vector<int>& A, int l, int r){
+    vector<int> temp(A.size());
+
+    for(int i = l; i <= r; i++){
+        temp[i] = A[i];
+    }
+
+    int m = (l+r)/2;
+
+    int i1 = l;
+    int i2 = m+1;
+
+    for(int curr = l; curr <= r; curr++){
+        //primeiro vou fazer as checagens se todos de alguma subarray ja foram copiados
+        //para isso vejo se alguma das variaveis auxiliares ja sairam de seu escopo designado
+        if(i2 > r){
+            //eh pq copiou tudo de i2
+            A[curr] = temp[i1];
+            i1++;
+        }
+
+        else if(i1 > m){
+            //eh pq copiou tudo de i1
+            A[curr] = temp[i2];
+            i2++;
+        }
+
+        else if(temp[i1] <= temp[i2]){
+            A[curr] = temp[i1];
+            i1++;
+        }
+
+        else{
+            A[curr] = temp[i2];
+            i2++;
+        }
+    }
+}
